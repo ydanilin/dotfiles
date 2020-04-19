ecdd2e11ccc51f0da1d9d3a5fba3e74be2164b8b
Yury Danilin <yuvede@gmail.com>
2020-04-19 09:19:34 +0300

    i3 config corrected + gnome-terminal settings

--- install-packages.sh
+++ install-packages.sh
@@ -2,6 +2,17 @@
 
 sudo apt-get update
 
+# rofi
+if ! type -p rofi > /dev/null; then
+    sudo apt -y install rofi
+fi
+
+# jq
+if ! type -p jq > /dev/null; then
+    sudo apt -y install jq
+fi
+
+# make
 if ! type -p make > /dev/null; then
     sudo apt -y install make
 fi
@@ -32,3 +43,16 @@ if ! type -p xkb-switch > /dev/null; then
     sudo ldconfig
     i3-msg -t command restart
 fi
+
+# gnome-terminal settings
+dconf write /org/gnome/terminal/legacy/default-show-menubar false
+dconf write /org/gnome/terminal/legacy/menu-accelerator-enabled false
+profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+profile=${profile:1:-1}
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ exit-action 'close'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ use-theme-colors false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ use-theme-transparency false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ background-color 'rgb(0,0,0)'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ foreground-color 'rgb(0,255,0)'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ palette "['rgb(46,52,54)', 'rgb(204,0,0)', 'rgb(78,154,6)', 'rgb(196,160,0)', 'rgb(52,101,164)', 'rgb(117,80,123)', 'rgb(6,152,154)', 'rgb(211,215,207)', 'rgb(85,87,83)', 'rgb(239,41,41)', 'rgb(138,226,52)', 'rgb(252,233,79)', 'rgb(114,159,207)', 'rgb(173,127,168)', 'rgb(52,226,226)', 'rgb(238,238,236)']"
+
