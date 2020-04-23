1cc0a47e5e10ffb2ef0d31cb06116d33a70856e4
Yury Danilin <yuvede@gmail.com>
2020-04-23 07:05:41 +0300

    Ranger + selected_editor

--- install-packages.sh
+++ install-packages.sh
@@ -56,3 +56,15 @@ gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profi
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ foreground-color 'rgb(0,255,0)'
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/ palette "['rgb(46,52,54)', 'rgb(204,0,0)', 'rgb(78,154,6)', 'rgb(196,160,0)', 'rgb(52,101,164)', 'rgb(117,80,123)', 'rgb(6,152,154)', 'rgb(211,215,207)', 'rgb(85,87,83)', 'rgb(239,41,41)', 'rgb(138,226,52)', 'rgb(252,233,79)', 'rgb(114,159,207)', 'rgb(173,127,168)', 'rgb(52,226,226)', 'rgb(238,238,236)']"
 
+# ranger
+if ! type -p xsel > /dev/null; then
+    sudo apt -y install xsel
+fi
+if ! type -p ranger > /dev/null; then
+    sudo apt -y install ranger
+fi
+
+# vim
+if ! type -p vim > /dev/null; then
+    sudo apt -y install vim
+fi
